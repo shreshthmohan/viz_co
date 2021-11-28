7d5446f88c21d4ea30c52fcb24e8bab10dd7cc88
Shreshth Mohan <sremog@gmail.com>
2021-11-28 17:24:41 +0530

    line band scatter - validation wip

--- src/charts/line_band_scatter/validateAndRender.js
+++ src/charts/line_band_scatter/validateAndRender.js
@@ -1,4 +1,8 @@
 import * as d3 from 'd3'
+import {
+  shouldBeNumber,
+  shouldNotBeBlank,
+} from '../../utils/validation/dataValidations'
 import { renderChart } from './render'
 
 export const validateAndRender = ({
@@ -7,6 +11,26 @@ export const validateAndRender = ({
   dimensions,
   chartContainerSelector,
 }) => {
+  const yFieldsDimensionTypes = {}
+  const yFieldDimensions = {}
+
+  dimensions.yFields.forEach((yf, i) => {
+    if (yf.line) {
+      yFieldsDimensionTypes[`__yField${i}_line__`] = [shouldBeNumber]
+      yFieldDimensions[`__yField${i}_line__`] = yf.line
+    }
+    // if (yf.band) {
+    //   yFieldsDimensionTypes[`__yField${i}_line__`] = [shouldBeNumber]
+    // }
+  })
+
+  const dimensionTypes = {
+    xField: [shouldNotBeBlank],
+    ...yFieldsDimensionTypes,
+  }
+
+  const flatDimensions = {}
+
   const dataFetchPromises = []
   dataPaths.forEach(dataPath => {
     dataFetchPromises.push(d3.csv(dataPath))
