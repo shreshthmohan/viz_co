207bbb39f05046703b32afc6c4ab213f51355442
Shreshth Mohan <sremog@gmail.com>
2021-12-03 16:17:47 +0530

    overlapping area - revert rollup min, already in dev

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
 import resolve from '@rollup/plugin-node-resolve'
-// terser will minify the bundle
-// import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './src/index.js',
@@ -19,9 +17,6 @@ export default {
     // not sure if cjs will work if d3 doesn't support cjs
     // { file: 'dist/bundle.cjs.js', format: 'cjs' },
   ],
-  plugins: [
-    resolve(),
-    //, terser()
-  ],
+  plugins: [resolve()],
   external: ['lodash-es', 'd3', 'd3-sankey'],
 }
